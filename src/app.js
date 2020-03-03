b4aad528eddb070c7d65b38822c10a5ce653ffe5
Meg Dahlgren <meg.dahlgren@gmail.com>
2020-03-03 12:05:34 -0500

    install dependencies and spin up crude express server

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+const port = 8080
+
+app
+  .get('/', (req, res) => {
+    res.send('Hello!')
+  })
+
+
+app.listen(port, () => console.log(`Sipulation app listening on port ${port}`))
